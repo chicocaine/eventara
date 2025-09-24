46482657663ef6a84beca28a4cac1f8f711ff027
chicocaine <eduardanthony1208@gmail.com>
2025-09-24 13:18:13 +0800

    Initial setup

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1 className="text-3xl font-bold underline">Hello from React + Tailwind!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
