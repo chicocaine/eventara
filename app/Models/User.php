f5607ea80bb92d5d5e249c46e6f44f2985ebe96f
chicocaine <eduardanthony1208@gmail.com>
2025-09-28 17:53:24 +0800

    user primaryKey bug fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,19 +8,17 @@ namespace App\Models;
 
 class User extends UserAuth
 {
-    // This class simply extends UserAuth to maintain compatibility
-    // with Laravel's default expectations for authentication
-    
     /**
-     * Create a new User instance (actually UserAuth).
-     * This is here for backward compatibility only.
+     * The table associated with the model.
+     *
+     * @var string
      */
-    public function __construct(array $attributes = [])
-    {
-        // Redirect to UserAuth table
-        $this->setTable('users_auth');
-        $this->setPrimaryKey('user_id');
-        
-        parent::__construct($attributes);
-    }
+    protected $table = 'users_auth';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'user_id';
 }
